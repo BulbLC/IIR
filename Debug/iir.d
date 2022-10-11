3e043806179ed9c490e2a4a81f738da18b95dbde
MigoXV <1695587905@qq.com>
2022-10-11 17:26:41 +0800

    IIR

---
+++ Debug/iir.d
@@ -0,0 +1,27 @@
+# FIXED
+
+iir.obj: ../iir.c
+iir.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/math.h
+iir.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/float.h
+iir.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/access.h
+iir.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/elfnames.h
+iir.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/mathf.h
+iir.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/mathl.h
+iir.obj: D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/unaccess.h
+
+../iir.c:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/math.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/float.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/access.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/elfnames.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/mathf.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/mathl.h:
+
+D:/Code\ Composer\ Studio/ccs/tools/compiler/c5500_4.4.1/include/unaccess.h:
+
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../ICETEK-VC5509-A.cmd 
+
+C_SRCS += \
+../iir.c 
+
+C_DEPS += \
+./iir.d 
+
+OBJS += \
+./iir.obj 
+
+OBJS__QUOTED += \
+"iir.obj" 
+
+C_DEPS__QUOTED += \
+"iir.d" 
+
+C_SRCS__QUOTED += \
+"../iir.c" 
+
+
